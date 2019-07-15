5278e1c1ea146220417f5678a1801f41c980f824
Callionica <16260283+callionica@users.noreply.github.com>
2019-07-15 16:38:04 +0100

    Update container-script.js

--- container-script.js
+++ container-script.js
@@ -65,9 +65,52 @@ function updateLatestLink() {
 	}
 }
 
+function focus(direction) {
+	var next = (direction === "forward");
+	var nextElement = document.querySelector("a");
+	var currentElement = document.activeElement;
+	if (currentElement) {
+		var targets = [...document.querySelectorAll("a")];
+		var index = targets.indexOf(currentElement);
+		if (index < targets.length) {
+			if ((index == 0) && !next) {
+				index = targets.length;
+			} else if ((index == targets.length - 1) && next) {
+				index = -1;
+			}
+			nextElement = targets[next ? (index + 1) : (index - 1)];
+		}
+	}
+
+	if (nextElement) {
+		nextElement.focus();
+	}
+}
+
 function init() {
 	updateLatestLink();
+	if (latestLink) {
+		latestLink.focus();
+	}
 	window.setInterval(updateLatestLink, 1 * 1000); // polling local storage every second
+
+	document.onkeydown = function onkeydown(evt) {
+		evt = evt || window.event;
+		var handled = false;
+
+		if (evt.key === "ArrowDown") {
+			handled = true;
+			focus("forward");
+		} else if (evt.key === "ArrowUp") {
+			handled = true;
+			focus("back");
+		}
+
+		if (handled) {
+			evt.stopPropagation();
+			evt.preventDefault();
+		}
+	}
 }
 
 ready(init);
